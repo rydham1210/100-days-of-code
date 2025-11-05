2c6c317b30b687c3eb9ea867407e756e53396062
rydham1210 <rydhamsingla4415.rs@gmail.com>
2025-11-06 01:39:07 +0530

    Add files via upload

---
+++ DAY25/second.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main()
+{
+    int a=5;
+    for(int i=1;i<=5;i++)
+    {
+        for (int j=a;j<=5;j++)
+      {
+         printf("%d",j);
+      }
+      a--;
+      printf("\n");
+    }
+    return 0;
+}
