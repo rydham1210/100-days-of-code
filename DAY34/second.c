d8ea57dc4ce9cfe13b82dcd2c5e51f29a9129f55
rydham1210 <rydhamsingla4415.rs@gmail.com>
2025-11-06 01:59:51 +0530

    Add files via upload

---
+++ DAY34/second.c
@@ -0,0 +1,30 @@
+
+#include<stdio.h>
+
+int main() 
+{
+    int n,index;
+    printf("Enter the number of elements in the array :");
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the %d entry of the array :",i);
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the index of element you want to delete:");
+    scanf("%d",&index);
+    
+    for(int i=index;i<n-1;i++)
+    {
+        a[i]=a[i+1];
+    }
+    n=n-1;
+    
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]);
+    }
+    
+    return 0;
+}
